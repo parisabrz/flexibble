b928ba867dcdfe1814c61aa6a01a6eed639e84c6
parisabrz <49725111+parisabrz@users.noreply.github.com>
2023-07-10 11:09:42 +0330

    add google auth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,8 +1,46 @@
+"use client"
+
+import { getProviders, signIn } from 'next-auth/react';
+import React, { useEffect, useState } from 'react'
+
+import Button from './Button';
+import { log } from 'console';
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams?: Record<string, string> | undefined;
+  };
+  
+  type Providers = Record<string, Provider>;
+
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
+    const [providers, setProviders] = useState<Providers | null>(null);
+
+    useEffect(() => {
+        const fetchProviders = async () => {
+            const res = await getProviders();
+    
+            setProviders(res);
+        }
+
+        fetchProviders();
+    }, []);
+console.log(providers);
+
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, i) => (
+                    <button key={i} title='Sign In' onClick={() => signIn(provider?.id)} />
+                ))}
+            </div>
+        )
+    }
 }
 
 export default AuthProviders
